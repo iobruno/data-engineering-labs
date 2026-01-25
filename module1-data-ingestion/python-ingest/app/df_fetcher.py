0abf85ca6342cfb05a9e836a187420eb52f4e652
Bruno Oliveira <bruno.ariev@gmail.com>
2026-01-25 18:35:47 -0300

    chore: update module1-pyingest (#148)
    * drying up codebase
    * drops ingest-app service from compose.yaml
    * applying ruff formatting and linting
    * update project dependencies

--- module1-data-ingestion/python-ingest/app/df_fetcher.py
+++ module1-data-ingestion/python-ingest/app/df_fetcher.py
@@ -1,44 +1,37 @@
 from abc import ABCMeta, abstractmethod
-from collections import namedtuple
 
 import pandas as pd
 import polars as pl
 
-Record = namedtuple("Record", ["url", "slices"])
-
 
 class DataframeFetcher(metaclass=ABCMeta):
-    schema: str = None
-    renaming_strategy: dict = {}
+    def with_schema(self, schema: dict):
+        def fetch_csv(endpoint: str) -> pl.DataFrame:
+            return self.fetch_csv(endpoint, schema)
 
-    def fetch_all(self, endpoints: list[str]) -> list[Record]:
-        for endpoint in endpoints:
-            yield self.fetch(endpoint)
-
-    def with_schema(self, schema) -> "DataframeFetcher":
-        self.schema = schema
-        return self
-
-    def with_renaming_strategy(self, renaming) -> "DataframeFetcher":
-        self.renaming_strategy = renaming
-        return self
+        return fetch_csv
 
     @abstractmethod
-    def fetch(self, endpoint: str) -> Record:
+    def fetch_csv(self, endpoint: str, schema: dict = None) -> pl.DataFrame | pd.DataFrame:
         raise NotImplementedError()
 
     @abstractmethod
-    def slice_df_in_chunks(self, df, chunk_size: int = 100_000) -> list[pd.DataFrame]:
+    def fetch_parquet(self, endpoint: str) -> pl.DataFrame | pd.DataFrame:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def slice_in_chunks(self, df, chunk_size: int = 100_000) -> list[pl.DataFrame | pd.DataFrame]:
         raise NotImplementedError()
 
 
 class PolarsFetcher(DataframeFetcher):
-    def fetch(self, endpoint: str) -> Record:
-        df = pl.read_csv(endpoint, schema_overrides=self.schema)
-        df = df.rename(self.renaming_strategy)
-        return Record(endpoint, self.slice_df_in_chunks(df))
+    def fetch_csv(self, endpoint: str, schema: dict = None) -> pl.DataFrame:
+        return pl.read_csv(endpoint, schema_overrides=schema)
 
-    def slice_df_in_chunks(self, df, chunk_size: int = 100_000) -> list[pl.DataFrame]:
+    def fetch_parquet(self, endpoint: str) -> pl.DataFrame:
+        return pl.read_parquet(endpoint)
+
+    def slice_in_chunks(self, df, chunk_size: int = 100_000) -> list[pl.DataFrame]:
         return [
             df.slice(offset=chunk_id, length=chunk_size)
             for chunk_id in range(0, len(df), chunk_size)
@@ -46,13 +39,13 @@ class PolarsFetcher(DataframeFetcher):
 
 
 class PandasFetcher(DataframeFetcher):
-    def fetch(self, endpoint: str) -> Record:
-        df = pd.read_csv(endpoint, engine="pyarrow", dtype=self.schema)
-        df = df.rename(columns=self.renaming_strategy)
-        return Record(endpoint, self.slice_df_in_chunks(df))
+    def fetch_csv(self, endpoint: str, schema: dict = None) -> pd.DataFrame:
+        return pd.read_csv(endpoint, engine="pyarrow", dtype=schema)
 
-    def slice_df_in_chunks(self, df, chunk_size: int = 100_000) -> list[pd.DataFrame]:
+    def fetch_parquet(self, endpoint: str) -> pd.DataFrame:
+        return pd.read_parquet(endpoint)
+
+    def slice_in_chunks(self, df, chunk_size: int = 100_000) -> list[pd.DataFrame]:
         return [
-            df.iloc[chunk_id : chunk_id + chunk_size]
-            for chunk_id in range(0, len(df), chunk_size)
+            df.iloc[chunk_id : chunk_id + chunk_size] for chunk_id in range(0, len(df), chunk_size)
         ]
