2b2b9fb18a80aeaf5f398041a0aa3a4648bd248e
iobruno <bruno.ariev@gmail.com>
2026-02-10 00:39:10 -0300

    Updates macro
    * Adds exhaustive clause (else) on resolve_payment_type_desc
    * Minor update on var name

--- module4-analytics-engineering/duckdb/macros/resolve_destination_schema.sql
+++ module4-analytics-engineering/duckdb/macros/resolve_destination_schema.sql
@@ -8,10 +8,10 @@
 {% macro resolve_type(model_type='staging') -%}
 
     {%- set target_env_var = 'DBT_DUCKDB_TARGET_SCHEMA'  -%}
-    {%- set stging_env_var = 'DBT_DUCKDB_STAGING_SCHEMA' -%}
+    {%- set staging_env_var = 'DBT_DUCKDB_STAGING_SCHEMA' -%}
 
     {%- if model_type == 'core' -%} {{- env_var(target_env_var, 'main') -}}
-    {%- else -%}                    {{- env_var(stging_env_var, 'main') -}}
+    {%- else -%}                    {{- env_var(staging_env_var, 'main') -}}
     {%- endif -%}
 
 {%- endmacro %}
